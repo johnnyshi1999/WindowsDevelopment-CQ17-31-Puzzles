3b0d8ef67bf091e0ee90bd644c1822aa42c566dd
quanghuyabc9 <quanghuyabc9@gmail.com>
2019-12-07 09:46:48 +0700

    Added move piece by up, left keyboard, set timer count down

--- WindowsDevelopment-CQ17-31-Puzzles/MainWindow.xaml.cs
+++ WindowsDevelopment-CQ17-31-Puzzles/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WindowsDevelopment_CQ17_31_Puzzles
 {
@@ -25,8 +26,11 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
     public partial class MainWindow : Window
     {
         PuzzleMaker maker;
-        Timer timer;
-        int count = 60 * 3;
+        //Timer timer;
+        //int count = 60 * 3;
+
+        DispatcherTimer _timer;
+        TimeSpan _time;
 
         public MainWindow()
         {
@@ -58,11 +62,28 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
             w_fix = colWidth - lineWeight;
 
             maker = PuzzleMaker.GetInstance();
-            timer = new Timer();
-            timer.Interval = 1000;
-            timer.Elapsed += Timer_Elapsed;
+            //timer = new Timer();
+            //timer.Interval = 1000;
+            //timer.Elapsed += Timer_Elapsed;
 
-            drawUI();
+            //_time = TimeSpan.FromMinutes(3);
+            //_time = TimeSpan.FromSeconds(3);
+
+            //_timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, 
+            //    delegate
+            //    {
+            //        LabelTimer.Content = _time.ToString(@"mm\:ss");
+            //        if (_time == TimeSpan.Zero)
+            //        {
+            //            System.Windows.MessageBox.Show("Time up", "You lose", MessageBoxButton.OK, MessageBoxImage.Information);
+            //            _timer.Stop();
+            //        }
+            //        _time = _time.Add(TimeSpan.FromSeconds(-1));
+            //    }, Application.Current.Dispatcher);
+
+            //_timer.Start();
+
+            //drawUI();
         }
 
         private void NewGame_Click(object sender, RoutedEventArgs e)
@@ -84,6 +105,23 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
                 leftBottomCanvas.Children.Clear();
                 drawUI();
                 SetupPieces(screen.FileName);
+                //Set time
+                _time = TimeSpan.FromMinutes(3);
+
+                _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal,
+                    delegate
+                    {
+                        LabelTimer.Content = _time.ToString(@"mm\:ss");
+                        if (_time == TimeSpan.Zero)
+                        {
+                            System.Windows.MessageBox.Show("Time up", "You lose", MessageBoxButton.OK, MessageBoxImage.Error);
+                            _timer.Stop();
+                        }
+                        _time = _time.Add(TimeSpan.FromSeconds(-1));
+                    }, Application.Current.Dispatcher);
+
+                _timer.Start();
+
             }
         }
 
@@ -180,67 +218,125 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            switch (e.Key)
+            if (!isDragging)
             {
-                case Key.Left:
-               
-                    break;
-                case Key.Right:
-                    Tuple<int, int> emptyPiece_KeyRight = maker.GetPiecePosition(8);
+                switch (e.Key)
+                {
+                    case Key.Left:
+                        Tuple<int, int> emptyPiece_KeyLeft = maker.GetPiecePosition(8);
 
-                    int i_KeyRight = emptyPiece_KeyRight.Item1;
-                    int j_KeyRight = emptyPiece_KeyRight.Item2 - 1;
-                    if (j_KeyRight >= 0 && j_KeyRight <= 2)
-                    {
-                        
-                        foreach (var child in leftBottomCanvas.Children)
+                        int i_KeyLeft = emptyPiece_KeyLeft.Item1;
+                        int j_KeyLeft = emptyPiece_KeyLeft.Item2 + 1;
+                        if (j_KeyLeft >= 0 && j_KeyLeft <= 2)
                         {
-                            if (child is Image)
+                            foreach (var child in leftBottomCanvas.Children)
                             {
-                                if ((int)(child as Image).Tag == maker.PuzzleTags[i_KeyRight, j_KeyRight])
+                                if (child is Image)
                                 {
-                                    selectedBitmap = child as Image;
-                                    break;
+                                    if ((int)(child as Image).Tag == maker.PuzzleTags[i_KeyLeft, j_KeyLeft])
+                                    {
+                                        selectedBitmap = child as Image;
+                                        break;
+                                    }
                                 }
                             }
-                        }
-                     
-                        int from = startX + lineWeight / 2 + j_KeyRight * (h_fix + lineWeight);
-                        int to = startX + lineWeight / 2 + (j_KeyRight + 1) * (h_fix + lineWeight);
-                        movePieceAnimation(from, to, 1);
-                        maker.MovePiece(new Tuple<int, int>(i_KeyRight, j_KeyRight), new Tuple<int, int>(i_KeyRight, j_KeyRight + 1));
-                    }
-                    break;
-                case Key.Up:
-                   
-                    break;
-                case Key.Down:
-                    Tuple<int, int> emptyPiece_KeyDown = maker.GetPiecePosition(8);
 
-                    int i_KeyDown = emptyPiece_KeyDown.Item1 - 1;
-                    int j_KeyDown = emptyPiece_KeyDown.Item2;
-                    if (i_KeyDown >= 0 && i_KeyDown <= 2)
-                    {
-                        //Find piece to move      
-                        foreach (var child in leftBottomCanvas.Children)
+                            int from = startX + lineWeight / 2 + j_KeyLeft * (h_fix + lineWeight);
+                            int to = startX + lineWeight / 2 + (j_KeyLeft - 1) * (h_fix + lineWeight);
+                            movePieceAnimation(from, to, 1);
+                            maker.MovePiece(new Tuple<int, int>(i_KeyLeft, j_KeyLeft), new Tuple<int, int>(i_KeyLeft, j_KeyLeft - 1));
+                            if (maker.CheckWin())
+                                System.Windows.MessageBox.Show("Congratulation", "You win", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+
+                        break;
+                    case Key.Right:
+                        Tuple<int, int> emptyPiece_KeyRight = maker.GetPiecePosition(8);
+
+                        int i_KeyRight = emptyPiece_KeyRight.Item1;
+                        int j_KeyRight = emptyPiece_KeyRight.Item2 - 1;
+                        if (j_KeyRight >= 0 && j_KeyRight <= 2)
                         {
-                            if (child is Image)
+
+                            foreach (var child in leftBottomCanvas.Children)
                             {
-                                if ((int)(child as Image).Tag == maker.PuzzleTags[i_KeyDown, j_KeyDown])
+                                if (child is Image)
                                 {
-                                    selectedBitmap = child as Image;
-                                    break;
+                                    if ((int)(child as Image).Tag == maker.PuzzleTags[i_KeyRight, j_KeyRight])
+                                    {
+                                        selectedBitmap = child as Image;
+                                        break;
+                                    }
                                 }
                             }
-                        }
 
-                        //Set up and move piece
-                        int from = startY + lineWeight / 2 + i_KeyDown * (h_fix + lineWeight);
-                        int to = startY + lineWeight / 2 + (i_KeyDown + 1) * (h_fix + lineWeight);
-                        movePieceAnimation(from, to, 3);
-                        maker.MovePiece(new Tuple<int, int>(i_KeyDown, j_KeyDown), new Tuple<int, int>(i_KeyDown + 1, j_KeyDown));
-                    }
-                    break;
+                            int from = startX + lineWeight / 2 + j_KeyRight * (h_fix + lineWeight);
+                            int to = startX + lineWeight / 2 + (j_KeyRight + 1) * (h_fix + lineWeight);
+                            movePieceAnimation(from, to, 1);
+                            maker.MovePiece(new Tuple<int, int>(i_KeyRight, j_KeyRight), new Tuple<int, int>(i_KeyRight, j_KeyRight + 1));
+                            if (maker.CheckWin())
+                                System.Windows.MessageBox.Show("Congratulation", "You win", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        break;
+                    case Key.Up:
+                        Tuple<int, int> emptyPiece_KeyUp = maker.GetPiecePosition(8);
+
+                        int i_KeyUp = emptyPiece_KeyUp.Item1 + 1;
+                        int j_KeyUp = emptyPiece_KeyUp.Item2;
+                        if (i_KeyUp >= 0 && i_KeyUp <= 2)
+                        {
+                            //Find piece to move      
+                            foreach (var child in leftBottomCanvas.Children)
+                            {
+                                if (child is Image)
+                                {
+                                    if ((int)(child as Image).Tag == maker.PuzzleTags[i_KeyUp, j_KeyUp])
+                                    {
+                                        selectedBitmap = child as Image;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            //Set up and move piece
+                            int from = startY + lineWeight / 2 + i_KeyUp * (h_fix + lineWeight);
+                            int to = startY + lineWeight / 2 + (i_KeyUp - 1) * (h_fix + lineWeight);
+                            movePieceAnimation(from, to, 2);
+                            maker.MovePiece(new Tuple<int, int>(i_KeyUp, j_KeyUp), new Tuple<int, int>(i_KeyUp - 1, j_KeyUp));
+                            if (maker.CheckWin())
+                                System.Windows.MessageBox.Show("Congratulation", "You win", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        break;
+                    case Key.Down:
+                        Tuple<int, int> emptyPiece_KeyDown = maker.GetPiecePosition(8);
+
+                        int i_KeyDown = emptyPiece_KeyDown.Item1 - 1;
+                        int j_KeyDown = emptyPiece_KeyDown.Item2;
+                        if (i_KeyDown >= 0 && i_KeyDown <= 2)
+                        {
+                            //Find piece to move      
+                            foreach (var child in leftBottomCanvas.Children)
+                            {
+                                if (child is Image)
+                                {
+                                    if ((int)(child as Image).Tag == maker.PuzzleTags[i_KeyDown, j_KeyDown])
+                                    {
+                                        selectedBitmap = child as Image;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            //Set up and move piece
+                            int from = startY + lineWeight / 2 + i_KeyDown * (h_fix + lineWeight);
+                            int to = startY + lineWeight / 2 + (i_KeyDown + 1) * (h_fix + lineWeight);
+                            movePieceAnimation(from, to, 2);
+                            maker.MovePiece(new Tuple<int, int>(i_KeyDown, j_KeyDown), new Tuple<int, int>(i_KeyDown + 1, j_KeyDown));
+                            if (maker.CheckWin())
+                                System.Windows.MessageBox.Show("Congratulation", "You win", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        break;
+                }
             }
         }
         /// <summary>
@@ -261,7 +357,7 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
 
             animation.From = from;
             animation.To = to;
-            animation.Duration = new Duration(TimeSpan.FromSeconds(1));
+            animation.Duration = new Duration(TimeSpan.FromSeconds(0.5));
 
             var story = new Storyboard();
 
@@ -274,14 +370,9 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
                     Storyboard.SetTargetProperty(animation, new PropertyPath(Canvas.LeftProperty));
                     break;
                 case 2:
-                    Storyboard.SetTargetProperty(animation, new PropertyPath(Canvas.RightProperty));
-                    break;
-                case 3:
                     Storyboard.SetTargetProperty(animation, new PropertyPath(Canvas.TopProperty));
                     break;
-                case 4:
-                    Storyboard.SetTargetProperty(animation, new PropertyPath(Canvas.BottomProperty));
-                    break;
+     
             }
             story.Begin(this);
         }
@@ -293,10 +384,16 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
             int i = (int)(position.Y - startY - leftTopCanvas.ActualHeight) / rowHeight;
             int j = (int)(position.X - startX) / colWidth;
 
-            if (i >= 0 && i <= rows - 1  && j >= 0 && j <= cols - 1)
+            if (i >= 0 && i <= rows - 1  && j >= 0 && j <= cols - 1 && isMoveValid())
             {
                 Canvas.SetLeft(selectedBitmap, startX + lineWeight / 2 + j * (w_fix + lineWeight));
                 Canvas.SetTop(selectedBitmap, startY + lineWeight / 2 + i * (h_fix + lineWeight));
+                maker.MovePiece(
+                    new Tuple<int, int>((int)(oldPosition_piece.Y - startY - leftTopCanvas.ActualHeight) / rowHeight, (int)(oldPosition_piece.X - startX) / colWidth),
+                    new Tuple<int, int>(i, j)
+                    );
+                if (maker.CheckWin())
+                    System.Windows.MessageBox.Show("Congratulation", "You win", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             // Snap to old position if piece is out of range
             else
@@ -309,21 +406,37 @@ namespace WindowsDevelopment_CQ17_31_Puzzles
             //movePieceAnimation(0, 800, 1);
         }
 
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        private bool isMoveValid()
         {
-            count--;
-            if (count == 0)
-            {
-                count = 60 * 3;
-                bool isWIn = maker.CheckWin();
-
-                if (isWIn != true)
-                {
-                    MessageBox.Show("Time out!!! You lose");
-                }
-                timer.Stop();
-            }
+           
+            Tuple<int, int> emptyPiece = maker.GetPiecePosition(8);
+            Tuple<int, int> selectedBitmap_Position = maker.GetPiecePosition((int)selectedBitmap.Tag);
+            if (emptyPiece.Item1 - 1 == selectedBitmap_Position.Item1 && emptyPiece.Item2 == selectedBitmap_Position.Item2)
+                return true;
+            if (emptyPiece.Item1 + 1 == selectedBitmap_Position.Item1 && emptyPiece.Item2 == selectedBitmap_Position.Item2)
+                return true;
+            if (emptyPiece.Item1 == selectedBitmap_Position.Item1 && emptyPiece.Item2 - 1 == selectedBitmap_Position.Item2)
+                return true;
+            if (emptyPiece.Item1 == selectedBitmap_Position.Item1 && emptyPiece.Item2 + 1 == selectedBitmap_Position.Item2)
+                return true;   
+            return false;
         }
+
+        //private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        //{
+        //    count--;
+        //    if (count == 0)
+        //    {
+        //        count = 60 * 3;
+        //        bool isWIn = maker.CheckWin();
+
+        //        if (isWIn != true)
+        //        {
+        //            MessageBox.Show("Time out!!! You lose");
+        //        }          
+        //        timer.Stop();
+        //    }
+        //}
 
         private void drawUI()
         {
